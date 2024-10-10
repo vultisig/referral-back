5a1aa656e44e3338c730be151ffe8f55b7719529
Gleb Ivanov <82587245+Exzik-dev@users.noreply.github.com>
2024-10-10 11:31:09 +0300

    add  message

--- src/external-api/external-api.service.ts
+++ src/external-api/external-api.service.ts
@@ -86,7 +86,7 @@ export class ExternalApiService {
             const res: AxiosResponse<VASUser> = await lastValueFrom(this.httpService.get(`${process.env.VAS_URL}${currentUser.wallet_public_key_ecdsa}/${currentUser.wallet_public_key_eddsa}`))
             return res.data
         } catch (e) {
-            throw new HttpException(e.response.data, 500)
+            throw new HttpException(e.message, 500)
         }
 
 
@@ -104,7 +104,7 @@ export class ExternalApiService {
                 status: res.data.join_airdrop
             }
         } catch (e) {
-            throw new HttpException(e.response.data, 500)
+            throw new HttpException(e.message, 500)
         }
 
 
